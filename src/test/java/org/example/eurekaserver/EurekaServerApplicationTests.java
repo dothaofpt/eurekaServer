f57925204a7f9a6d3e396d2790fc182875a3ac07
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-11-13 12:32:17 +0700

    up

--- src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
@@ -2,12 +2,14 @@ package org.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
+@EnableEurekaServer
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
